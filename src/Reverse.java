da9308cfc66411acbe5f163bf17d9713329c95af
alisherkhidirov <khidirovalisher@gmail.com>
2021-01-26 19:08:33 -0500

    revers changes1

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,2 +1,20 @@
 public class Reverse {
+
+    public static void main (String[] args) {
+
+        System.out.println(revers("Hello"));
+
+
+    }
+
+     public static String revers(String str) {
+        String empty = "";
+        for(int i = str.length()-1; i>=0; i--) {
+            empty +=str.charAt(i);
+        }
+            return empty;
+
+    }
+
+
 }
